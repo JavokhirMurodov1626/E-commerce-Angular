26e9b7552dac7c55751e6a16f63c7653aab8fa83
Oybek <oybeksalomov256@gmail.com>
2023-09-14 15:33:32 +0500

    add component (banner)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BannerComponent } from './banner/banner.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BannerComponent
+  ],
+  exports: [
+    BannerComponent
+  ],
   imports: [
     CommonModule
   ]

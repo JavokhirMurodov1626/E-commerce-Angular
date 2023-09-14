41368529cbfb814919ec97fb59fcee7fff7fc2ef
Oybek <oybeksalomov256@gmail.com>
2023-09-14 17:12:37 +0500

    add component (why-us)

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import {SharedModule} from "../../shared/shared.module";
+import { WhyUsComponent } from './components/why-us/why-us.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    WhyUsComponent,
   ],
     imports: [
         CommonModule,

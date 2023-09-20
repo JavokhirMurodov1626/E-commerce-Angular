17596b36dddb2d5baaac49173babd3c0d457dfa4
Javohir Murodov <javohir.murodov.9238@gmail.com>
2023-09-20 17:44:50 +0500

    Revert "Feature/banner why us"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BannerComponent } from './banner/banner.component';
-
 import { StarRatingComponent } from './components/star-rating/star-rating.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
-  declarations: [StarRatingComponent, ProductCardComponent, LoaderComponent, BannerComponent],
+  declarations: [StarRatingComponent, ProductCardComponent, LoaderComponent,],
   imports: [CommonModule],
-  exports: [StarRatingComponent,ProductCardComponent,LoaderComponent, BannerComponent],
-
+  exports: [StarRatingComponent,ProductCardComponent,LoaderComponent],
 })
 export class SharedModule {}

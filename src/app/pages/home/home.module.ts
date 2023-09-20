1d608e388147bb744f67d72bc2d8979cbd902dfa
Javohir Murodov <javohir.murodov.9238@gmail.com>
2023-09-20 17:36:07 +0500

    Revert "Feature/banner why us"

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,15 +5,13 @@ import { HttpClientModule } from  '@angular/common/http';
 import { NgToastModule } from 'ng-angular-popup'
 
 import { HomeComponent } from './components/home/home.component';
-import { WhyUsComponent } from './components/why-us/why-us.component';
 import { ProductsGridComponent } from './components/products-grid/products-grid.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 
+
 @NgModule({
-  declarations: [HomeComponent, ProductsGridComponent,HomeComponent,
-    WhyUsComponent,],
+  declarations: [HomeComponent, ProductsGridComponent],
   imports: [CommonModule, SharedModule,HttpClientModule, AppRoutingModule, NgToastModule],
   exports: [HomeComponent],
-
 })
 export class HomeModule {}

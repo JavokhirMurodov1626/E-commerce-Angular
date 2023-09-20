bb5097d4be788414260efe30ef876e2841ce3070
Muydinov Mubosher <muydinovmubosher@yandex.ru>
2023-09-20 09:20:48 +0500

    feature product add to home page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
+import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-  ],
-  imports: [
-    CommonModule,
-  ],
-  exports:[
-    HomeComponent,
-  ]
+  declarations: [HomeComponent, FeaturedProductsComponent],
+  imports: [CommonModule],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}

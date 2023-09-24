e33be47b97ea5c289ee10db1c2851a3808d42327
Oybek <oybeksalomov256@gmail.com>
2023-09-24 14:48:25 +0500

    edit cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,42 +1,54 @@
-import {Component, DoCheck, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {formatCurrency} from "@angular/common";
+import {Component, OnDestroy, OnInit,} from '@angular/core';
+import {CartService} from "./service/cart.service";
+import {CartProductModel} from "./service/cart.model";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit, DoCheck{
-  itemQuanty: number = 1;
-  itemPrice: number = 998
-  shippingFee: number = 20
-  vaucherCode: any = {first: 10}
-  itemUnitPrice: number = this.itemPrice;
-  checkOutTotal: number = this.itemUnitPrice;
+export class CartComponent implements OnInit, OnDestroy{
+  shippingFee: number = 2
+  vaucherCode: any = {}
+  cartProducts!: CartProductModel[];
+  checkOutTotal!: number;
+  subtotal!: number;
+  sub$ = new Subject()
 
-  constructor() {
+  constructor(private cartService: CartService) {
   }
-
-  calculateUnitPrice(operator: string) {
-    if (operator === 'plus') {
-      this.itemQuanty++
-    } else if (operator === 'minus' && this.itemQuanty > 1) {
-      this.itemQuanty--
-    }
-    this.itemUnitPrice = this.itemPrice * this.itemQuanty
-    this.calculateTotal()
-  }
-  calculateTotal() {
-    this.checkOutTotal = (this.itemUnitPrice * (1 - (this.vaucherCode.first ?? 0)/100)) + this.shippingFee
-
-  }
-
 
   ngOnInit() {
-    this.calculateTotal()
-  }
-  ngDoCheck() {
-    // console.log(this.itemQuanty)
+    this.cartService.cartProducts$
+      .pipe(takeUntil(this.sub$))
+      .subscribe(cartProducts => {
+        this.cartProducts = cartProducts
+      })
+    this.calculateUnitPrice()
   }
 
+  calculateUnitPrice(operator?: string, productIndex: number = 0) {
+    if (operator === 'plus') {
+      this.cartProducts[productIndex].quantity++
+    } else if (operator === 'minus' && this.cartProducts[productIndex].quantity > 1) {
+      this.cartProducts[productIndex].quantity--
+    }
+    this.calculateSubTotal()
+    this.calculateTotal()
+  }
+  calculateSubTotal() {
+    this.subtotal = 0
+    this.cartProducts.forEach(cartProduct => {
+      this.subtotal += (cartProduct.price * cartProduct.quantity)
+    })
+  }
+  calculateTotal() {
+    this.checkOutTotal = (this.subtotal * (1 - (this.vaucherCode.first ?? 0)/100)) + this.shippingFee
+  }
+
+  ngOnDestroy() {
+    this.sub$.next(null)
+    this.sub$.complete()
+  }
 }
